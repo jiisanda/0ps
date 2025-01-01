70275b93ab6fcf8e83932a4349e29eb02b71a759
jiisanda <harshjaiswal2307@gmail.com>
2025-01-01 23:10:16 +0530

    🤫🚀 private instance

--- tf/main.tf
+++ tf/main.tf
@@ -36,10 +36,53 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2b"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "private subnet"
+  }
+}
+
+# aws ec2 instance
+resource "aws_instance" "public_instance" {
+  ami               = "ami-07d9cf938edb0739b"
+  instance_type     = "t2.micro"
+  availability_zone = "us-west-2a"
+
+  key_name = "rootkey"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.netif.id
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install apache2 -y
+              sudo systemctl start apache2
+              sudo bash -c 'echo Deployed via Terraform' > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "public instance"
+  }
+}
+
+resource "aws_instance" "private_instance" {
+  ami               = "ami-07d9cf938edb0739b"
+  instance_type     = "t2.micro"
+  availability_zone = "us-west-2a"
+
+  key_name = "rootkey"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.private_netif.id
+  }
+
+  tags = {
+    Name = "private instance"
   }
 }
 
@@ -50,6 +93,18 @@ resource "aws_internet_gateway" "gw" {
   tags = {
     Name = "main vpc gateway"
   }
+}
+
+# nat gateway
+resource "aws_nat_gateway" "ngw" {
+  subnet_id     = aws_subnet.public.id
+  allocation_id = aws_eip.nat_eip.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  depends_on = [aws_internet_gateway.gw]
 }
 
 # route table
@@ -71,10 +126,28 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = "private route table"
+  }
+}
+
 # route table association to subnet
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
 
 # security groups
@@ -89,53 +162,59 @@ resource "aws_security_group" "secgrp" {
 }
 
 resource "aws_security_group_rule" "port_443" {
-  for_each          = toset(["0.0.0.0/0"])
+  for_each = toset(["0.0.0.0/0"])
   description       = "HTTPS traffic"
   type              = "ingress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = [each.value]
+  cidr_blocks = [each.value]
   security_group_id = aws_security_group.secgrp.id
 }
 
 resource "aws_security_group_rule" "port_80" {
   description       = "HTTP traffic"
-  for_each          = toset(["0.0.0.0/0"])
+  for_each = toset(["0.0.0.0/0"])
   from_port         = 80
   protocol          = "tcp"
   security_group_id = aws_security_group.secgrp.id
   to_port           = 80
   type              = "ingress"
-  cidr_blocks       = [each.value]
+  cidr_blocks = [each.value]
 }
 
 resource "aws_security_group_rule" "port_22" {
   description       = "HTTP traffic"
-  for_each          = toset(["0.0.0.0/0"])
+  for_each = toset(["0.0.0.0/0"])
   from_port         = 22
   protocol          = "tcp"
   security_group_id = aws_security_group.secgrp.id
   to_port           = 22
   type              = "ingress"
-  cidr_blocks       = [each.value]
+  cidr_blocks = [each.value]
 }
 
 resource "aws_security_group_rule" "egress_sg" {
   description       = "All traffic"
-  for_each          = toset(["0.0.0.0/0"])
+  for_each = toset(["0.0.0.0/0"])
   from_port         = 0
   protocol          = "-1"
   security_group_id = aws_security_group.secgrp.id
   to_port           = 0
   type              = "egress"
-  cidr_blocks       = [each.value]
+  cidr_blocks = [each.value]
 }
 
 # network interface
 resource "aws_network_interface" "netif" {
   subnet_id = aws_subnet.public.id
   private_ips = ["10.0.0.50"]
+  security_groups = [aws_security_group.secgrp.id]
+}
+
+resource "aws_network_interface" "private_netif" {
+  subnet_id = aws_subnet.private.id
+  private_ips = ["10.0.1.50"]
   security_groups = [aws_security_group.secgrp.id]
 }
 
@@ -147,28 +226,6 @@ resource "aws_eip" "one" {
   depends_on = [aws_internet_gateway.gw]
 }
 
-# aws ec2 instance
-resource "aws_instance" "public_instance" {
-  ami               = "ami-07d9cf938edb0739b"
-  instance_type     = "t2.micro"
-  availability_zone = "us-west-2a"
-
-  key_name          = "rootkey"
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.netif.id
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install apache2 -y
-              sudo systemctl start apache2
-              sudo bash -c 'echo Deployed via Terraform' > /var/www/html/index.html
-              EOF
-
-  tags = {
-    Name = "public instance"
-  }
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
 }
